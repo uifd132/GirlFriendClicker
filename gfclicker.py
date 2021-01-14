17e173eab1054f4bd72b900e5809412e393fd14e
moistsaxophone <zmccormack2151@gmail.com>
2021-01-14 14:10:48 -0800

    made window

--- gfclicker.py
+++ gfclicker.py
@@ -1,8 +1,24 @@
 import pygame
 pygame.init()
+print("Game loading lol if u see this ur gay lol omg lolololol terminal user lol")
 
-screenWidth = 1280
-screenHeight = 720
+screenWidth = 600
+screenHeight = 950
+running = True
 
 clock = pygame.time.Clock()
 win = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption("Girlfriend Clicker: The Game")
+
+#Images load here
+
+
+
+while running:
+
+    clock.tick(30)
+
+    #closes game
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
