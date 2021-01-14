70d5324cc9906365e799d51c812060888bae396f
moistsaxophone <zmccormack2151@gmail.com>
2021-01-14 14:52:39 -0800

    amazing is on the screen

--- gfclicker.py
+++ gfclicker.py
@@ -11,8 +11,25 @@ win = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Girlfriend Clicker: The Game")
 
 #Images load here
+bg = pygame.image.load('images/screen.png')
+amazingImage = pygame.image.load('images/amazing.png')
+
+class apps(object):
+    def __init__(self):
+        self.got = True
+
+    def drawAmazing(self,win):
+        if self.got:
+            win.blit(amazingImage,(55,157))
 
 
+def drawStuff():
+    win.blit(bg, (0,0))
+    amazing.drawAmazing(win)
+    pygame.display.update()
+
+
+amazing = apps()
 
 while running:
 
@@ -22,3 +39,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    drawStuff()
+
+pygame.quit
