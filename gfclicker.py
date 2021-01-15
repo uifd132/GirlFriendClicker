9c79e3e6049aa9bf5d8af403cb8ce16571d88975
uifd132 <76170929+uifd132@users.noreply.github.com>
2021-01-15 11:53:24 -0500

    Created the Nav bar and click counter
    So i created it and it all works, but i haven't figured out how to clear the old number to replace the new number. so if one of you could take a looksie at that. that would be poggers

--- gfclicker.py
+++ gfclicker.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import pygame.freetype
 from name_generator import gen_name
 from classes import apps
 pygame.init()
@@ -13,20 +14,34 @@ clock = pygame.time.Clock()
 win = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Girlfriend Clicker: The Game")
 
+#(this bit is for the font/scorecard)
+black = (0,0,0)
+font = pygame.freetype.Font('text/COMIC.TTF', 24)
+
 # Images load here
 border = pygame.image.load('images/apps/screen.png')
+notificationBar = pygame.image.load('images/apps/notificationBar.png')
 homeScreen = pygame.image.load('images/apps/bg.png')
 amazingImage = pygame.image.load('images/apps/amazing.png')
 messagesImage = pygame.image.load('images/apps/messages.png')
 lickrImage = pygame.image.load('images/apps/lickr.png')
 
+# Creates the int for the score and the scorecard to blit
+affection = 0
 
 # Draws the screen and objects
-
 def drawBorder():
     win.blit(border, (0,0))
+    win.blit(notificationBar, (0,0))
+    drawScoreCard()
     pygame.display.update()
 
+def drawScoreCard():
+    textSurface, rect = font.render(str(affection), (0,0,0))
+    win.blit(textSurface, (110,109))
+    print("REFRESHING SCORECARD")
+    pygame.display.update()
+    
 def drawHomeScreen():
     win.blit(wallpaper, (0,0))
     amazing.drawApps(win)
@@ -42,13 +57,18 @@ amazing = apps(238,157,amazingImage)
 messages = apps(55,157,messagesImage)
 lickr = apps(417,157,lickrImage)
 wallpaper = homeScreen
-
 drawHomeScreen()
 
 while running:
 
-    clock.tick(30)
-
+    clock.tick(1)
+    
+    #attempt to update scorecard
+    affection += 1
+    print(str(affection))
+    drawScoreCard()
+    
+    
     # Closes game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -58,7 +78,8 @@ while running:
     pos = pygame.mouse.get_pos()
     if event.type == pygame.MOUSEBUTTONDOWN:
         if amazing.isOver(pos):
-            print("clicked the button")
+            clearScreen()
+            
 
 
 pygame.quit()
